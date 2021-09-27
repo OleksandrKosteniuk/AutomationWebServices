ca93adf659f98ea0af8aa00c32bb80be46abdd67
OleksandrKosteniuk <Oleksandr_Kosteniuk@epam.com>
2021-09-27 10:46:10 +0300

    HW-27.09.2021-2

--- src/main/java/ProductLineItem.java
+++
@@ -1,5 +0,0 @@
-public class ProductLineItem {
-        public static String getProductLineItemBodyForRequest(int productId, int productQty){
-            return "{\"product\":{\"code\":\""+productId+"\"},\" quantity\":"+productQty+"}";
-        }
-}
--- src/test/java/Test01_GET.java
+++ src/test/java/Test01_GET.java
@@ -6,25 +6,30 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 import static org.hamcrest.Matchers.equalTo;
 import io.restassured.response.Response;
 import pages.ShoppingCartPage;
+import payload.Product;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Test01_GET {
-    ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
-    ShoppingCartPageApi shoppingCartPageApi = new ShoppingCartPageApi();
-    String guid = shoppingCartPageApi.getGuIdFromCreateShoppingCartPostApiCall();
+    
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+        ShoppingCartPageApi shoppingCartPageApi = new ShoppingCartPageApi();
+        Product product = new Product("5156733",1);
 
+        String guid = shoppingCartPageApi.getGuIdFromCreateShoppingCartPostApiCall();
+        
     @Test
     public void addProductToBasket() {
+        
         Response addProductToBasketResponse = given().
                 contentType("application/json").
                 accept("application/json").
-                body("{\"product\":{\"code\":\"5156733\"},\" quantity\":1}").
+                body(product.getAddProductToCardPayload()).
                 when().
                 post(shoppingCartPageApi.getAddProductToShoppingCartEndpoint(guid)).
                 then().
                 body(matchesJsonSchemaInClasspath("post.json")).
-                body(shoppingCartPageApi.getProductCodeJsonPath(), equalTo("5156733")).
-                body(shoppingCartPageApi.getProductQtyJsonPath(), equalTo(1)).
+                body(shoppingCartPageApi.getProductCodeJsonPath(), equalTo(product.getProductId())).
+                body(shoppingCartPageApi.getProductQtyJsonPath(), equalTo(product.getProductQty())).
                 statusCode(200).
                 extract().
                 response();
@@ -33,7 +38,7 @@ public class Test01_GET {
     @Test
     public void isProductAddedToShoppingCart(){
         shoppingCartPage.openShoppingCartPageByBrowser(guid);
-        assertThat(shoppingCartPage.getProductLineItemLink().getAttribute("href").contains("5156733"))
+        assertThat(shoppingCartPage.getProductLineItemLink().getAttribute("href").contains(product.getProductId()))
                 .overridingErrorMessage("Product is not added to Shopping Cart")
                 .isTrue();
     }
